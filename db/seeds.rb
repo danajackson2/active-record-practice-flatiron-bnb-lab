2c3122c878a87afc2e9e79f2d05c215268c64ec3
github username <github email address>
2020-12-01 16:25:41 -0800

    yay

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,21 @@
 require_relative '../config/environment.rb'
+require 'pry'
+l1 = Listing.create
+l2 = Listing.create
+
+rs1 = Reservation.create
+rs1.listing = l1
+rs2 = Reservation.create
+rs2.listing = l1
+
+rv1 = Review.create
+rv1.reservation = rs1
+rv1.save
+
+
+rv2 = Review.create
+rv2.reservation = rs2
+rv2.save
+
+binding.pry
+"as"
